b9237082d1bb7d8cc9746742ee30846b8d57a98c
mehrshad <eskandarimehrshad7@gmail.com>
2026-09-11 18:32:58 +0330

    Started connecting backend to frontend. Frontend loads from backend

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from decimal import Decimal
 from tracker import ExpenseTracker
@@ -40,6 +41,15 @@ months = {"1" : "January",
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 et = ExpenseTracker()
 
 @app.get("/expenses", response_model=list[ExpenseOut])
